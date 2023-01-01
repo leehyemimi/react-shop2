c8b288a939ab213e344b3955fd281cba0b34250a
테스트 <test@gmail.com>
2023-01-01 23:58:11 +0900

    ajax

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,21 +1,52 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 function Detail({ shoes }) {
-
 	let { id } = useParams();
 	// let item = shoes.find(function (x) {
 	// 	return x.id == id
 	// });
-	let item = shoes.find((x) => x.id == id)
+	let item = shoes.find((x) => x.id == id);
+	let [count, setCount] = useState(0);
+	let [alerttxt, setAlerttxt] = useState(true);
+	let [num, setNum] = useState('');
+
+	useEffect(() => {
+		let timer = setTimeout(() => { setAlerttxt(false) }, 2000)
+		return () => {	//useEffect 실행되기전에 //cleanup function
+			clearTimeout(timer);
+		}
+	}, []); //1회만 코드 실행
+
+	useEffect(() => {
+		if (isNaN(num) == true) {
+			alert('그러지 마세요');
+		}
+		return () => {	//useEffect 실행되기전에 //cleanup function
+			setNum(null);
+		}
+	}, [num])
 
 	return (
 		<div className="container">
+			{
+				alerttxt === true ?
+					<div className="alert alert-warning">
+						2초이내 구매시 할인
+					</div>
+					:
+					null
+			}
+
+			{/* {count}
+			<button onClick={() => { setCount(count + 1) }}></button> */}
 			<div className="row">
 				<div className="col-md-6">
 					<img src={`https://codingapple1.github.io/shop/shoes` + (item.id + 1) + `.jpg`} width="100%" alt="." />
 				</div>
 				<div className="col-md-6">
+					<div><input type="text" onChange={(e) => { setNum(e.target.value) }} onClick={(e) => { e.target.value = null }} /></div>
 					<h4 className="pt-5">{item.title}</h4>
 					<p>{item.content}</p>
 					<p>{item.price}</p>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,11 @@
 import { Container, Row } from 'react-bootstrap';
 import Card from "../components/Card";
+import axios from "axios";
+import { useState } from "react";
 
-function Main({ shoes }) {
+function Main({ shoes, setShoes }) {
+	let [more, setMore] = useState(1);
+	let [loding, setLoding] = useState(false)
 
 	return (
 		<>
@@ -13,8 +17,37 @@ function Main({ shoes }) {
 							<Card shoes={shoes[i]} i={i} key={i} />
 						)
 					})}
+
+					{
+						loding == true ?
+							<div className='loding'>
+								로딩중입니다...
+							</div>
+							:
+							null
+					}
 				</Row>
 			</Container>
+			<button onClick={() => {
+				setLoding(true);	//로딩중 UI띄우기
+				<div>로딩중</div>
+				setMore(more = more + 1);
+				if (more > 3) {
+					alert('더 이상 상품이 없습니다.');
+					setLoding(false);	//로딩중 UI숨기기
+				} else {
+					axios.get(`https://codingapple1.github.io/shop/data` + more + `.json`)
+						.then((data) => { //성공할때
+							let copy = [...shoes, ...data.data]
+							setShoes(copy);
+							setLoding(false);	//로딩중 UI숨기기
+						})
+						.catch(() => {
+							console.log('실패함ㅅㄱ');
+							setLoding(false);	//로딩중 UI숨기기
+						})
+				}
+			}}>버튼</button>
 		</>
 	);
 }
