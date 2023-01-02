e61748a1042e61bc8dea289f7de293f73d4541b4
테스트 <sunone222@gmail.com>
2023-01-02 23:00:08 +0900

    add

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Container, Row } from 'react-bootstrap';
 import axios from "axios";
+
 import Card from "../components/Card";
 
 function Main({ shoes, setShoes }) {
